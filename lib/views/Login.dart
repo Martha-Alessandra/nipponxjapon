4cfa5762b737ca594c188be15e061b5418672dc3
Marco <marco12_2@hotmail.com>
2024-10-01 22:10:13 -0600

    Revert "Merge branch 'master' into frontend"
    This reverts commit f1e3f699fa729455add437aeeb0219091f85ae30, reversing
    changes made to 5c0db4f62ae72818e00093e0d94d1803bcc2bafc.

--- lib/views/Login.dart
+++ lib/views/Login.dart
@@ -5,7 +5,7 @@ import 'package:proyecto_turismo/utils/auth.dart';
 import 'package:proyecto_turismo/views/Itinerario.dart';
 import 'package:flutter/services.dart';
 import 'package:proyecto_turismo/views/Registro.dart';
-import 'package:proyecto_turismo/widgets/Buttons_Login.dart';
+import 'package:proyecto_turismo/widgets/Buttons_Login';
 import 'package:proyecto_turismo/widgets/Textfield_Login.dart';
 
 class Login extends StatefulWidget {
@@ -82,50 +82,27 @@ class _LoginSate extends State<Login> {
               ),
               child: Column(
                 children: [
-                  Padding(
+                  TextfieldLogin(
+                    hintText: 'Correo', 
+                    icono: Icons.mail, 
                     padding: const EdgeInsets.only(
-                        bottom: 3, top: 20, left: 28, right: 28),
-                    child: Container(
-                      height: 45,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFF8E2BB),
-                        borderRadius: const BorderRadius.all(Radius.circular(9)),
-                        border: Border.all(
-                            color: const Color(0xFFF6DCAC), width: 1),
-                      ),
-                      child: TextField(
-                        controller: emailController, // Asociar el controlador
-                        decoration: const InputDecoration(
-                          hintText: 'Correo',
-                          border: InputBorder.none,
-                          prefixIcon: Icon(Icons.mail),
-                        ),
-                      ),
+                      bottom: 3, 
+                      top: 20, 
+                      left: 28, 
+                      right: 28
                     ),
+                    controller: emailController
                   ),
-                  Padding(
+                  TextfieldLogin(
+                    hintText: 'Contraseña', 
+                    icono: Icons.lock, 
                     padding: const EdgeInsets.only(
-                        bottom: 2, top: 5, left: 28, right: 28),
-                    child: Container(
-                      height: 45,
-                      width: 300,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFF8E2BB),
-                        borderRadius: const BorderRadius.all(Radius.circular(9)),
-                        border: Border.all(
-                            color: const Color(0xFFF6DCAC), width: 1),
-                      ),
-                      child: TextField(
-                        controller: passwordController, // Asociar el controlador
-                        decoration: const InputDecoration(
-                          hintText: 'Contraseña',
-                          border: InputBorder.none,
-                          prefixIcon: Icon(Icons.lock),
-                        ),
-                        obscureText: true,
-                      ),
+                      bottom: 2, 
+                      top: 5, 
+                      left: 28, 
+                      right: 28
                     ),
+                    controller: passwordController
                   ),
                   Row(
                     children: [
@@ -147,67 +124,51 @@ class _LoginSate extends State<Login> {
                       Container(
                         width: 15,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 12,
-                          bottom: 1,
-                          left: 1,
-                          right: 1,
-                        ),
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            if (emailController.text.isEmpty ||
-                                passwordController.text.isEmpty) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content:
-                                      Text('Por favor, complete todos los campos.'),
-                                  duration: Duration(seconds: 3),
-                                ),
-                              );
-                              return;
-                            }
+                      ButtonsLogin(
+                        texto: 'INICIAR SESION', 
+                        size: const Size(157, 42), 
+                        onPressed: () async {
+                          if (emailController.text.isEmpty ||
+                          passwordController.text.isEmpty) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content:
+                                  Text('Por favor, complete todos los campos.'),
+                                duration: Duration(seconds: 3),
+                              ),
+                            );
+                            return;
+                          }
 
-                            String email = emailController.text.trim();
-                            String password = passwordController.text.trim();
+                          String email = emailController.text.trim();
+                          String password = passwordController.text.trim();
 
-                            var result = await _authService
-                                .SignInEmailAndPassword(email, password);
+                          var result = await _authService
+                              .SignInEmailAndPassword(email, password);
 
-                            if (result == 1 || result == 2) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('Error en el usuario o contraseña'),
-                                  duration: Duration(seconds: 3),
-                                ),
-                              );
-                            } else if (result != null) {
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const Itinerario()),
-                              );
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text(
-                                      'Ocurrió un error. Inténtalo nuevamente.'),
-                                  duration: Duration(seconds: 3),
-                                ),
-                              );
-                            }
-                          },
-                          style: TextButton.styleFrom(
-                              backgroundColor: const Color(0xFFEF762F),
-                              foregroundColor: const Color(0xFFFFFFFF),
-                              textStyle: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 15.2),
-                              fixedSize: const Size(157, 42),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(12))),
-                          child: const Text('INICIAR SESION'),
-                        ),
+                          if (result == 1 || result == 2) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Error en el usuario o contraseña'),
+                                duration: Duration(seconds: 3),
+                              ),
+                            );
+                          } else if (result != null) {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const Itinerario()
+                              ),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Ocurrió un error. Inténtalo nuevamente.'),
+                                duration: Duration(seconds: 3),
+                              ),
+                            );
+                          }
+                        },
                       ),
                     ],
                   )
